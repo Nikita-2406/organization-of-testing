16dcdea151dd9f2e207ffc1f1f54c18801576e5c
Nikita-2406 <89524208686@mail.ru>
2024-07-18 11:49:03 +0300

    1.0.1

--- src/js/__tests__/widget.test.js
+++ src/js/__tests__/widget.test.js
@@ -1,12 +1,12 @@
 import { Widget } from "../widget";
 
-test('widget should render', () => {
+test("widget should render", () => {
   // document.body.innerHTML = '<div class="container"></div>';
 
-  const container = document.querySelector('.container');
+  const container = document.querySelector(".container");
   const widget = new Widget(container);
 
   widget.bindToDom();
 
-  expect(container.innerHTML).toEqual(InnFormWidget.markup);
-})
+  expect(container.innerHTML).toEqual(Widget.markup);
+});
